18fb52768887c64e65853f66cf185bf0b6ae4076
Dhanan <64773747+Dhanan24@users.noreply.github.com>
2021-05-10 02:30:12 +1200

    tried to get sign in page started

---
+++ app/Http/Controllers/SignInController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SignInController extends Controller
+{
+    public function homePage() {
+        return view('homepage.homePage');
+    }
+
+    public function welcome() {
+        return view('welcome');
+    }
+}
