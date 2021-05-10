5d58c6f6a8f0d3af457bc7b1890fa8c6e585ea0e
Dhanan <64773747+Dhanan24@users.noreply.github.com>
2021-05-10 14:56:34 +1200

    added topnav

--- resources/views/homepage/homePage.blade.php
+++ resources/views/homepage/homePage.blade.php
@@ -9,16 +9,81 @@
       float: left;
       color: #f2f2f2;
       text-align: center;
-      padding: 25px 25px;
+      padding: 17px 17px;
       text-decoration: none;
       font-size: 17px;
       font-family: 'Tahoma New', monospace;
+      vertical-align:bottom ;
     }
 
-    .topnav a.active {
-      background-color: #4A90E2;
-      color: white;
-    }
+
+    .logo {
+  float: left;
+}
+/* ~~ Top Navigation Bar ~~ */
+
+#navigation-container {
+  width: 1200px;
+  margin: 0 auto;
+  height: 70px;
+}
+
+.navigation-bar {
+  background-color: #352d2f;
+  height: 70px;
+  width: 100%;
+}
+
+.navigation-bar ul {
+  padding: 0px;
+  margin: 0px;
+  text-align: center;
+}
+
+.navigation-bar li {
+  list-style-type: none;
+  padding: 0px;
+  height: 24px;
+  margin-top: 4px;
+  margin-bottom: 4px;
+  display: inline;
+}
+
+.navigation-bar li a {
+  color: black;
+  font-size: 16px;
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  line-height: 70px;
+  padding: 5px 15px;
+  opacity: 0.7;
+}
+
+#menu {
+  float: right;
+}
+
+  .footer {
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: rgb(10, 21, 43);
+    color: white;
+    text-align: center;
+    position:static;
+   bottom:0;
+   height:60px;   /* Height of the footer */
+  }
+
+  body {
+    display: block;
+    margin: 0px;
+}
+
+img {
+  width: 29%;
+  /* object-fit: scale-down; */
+}
   </style>
 
   <meta charset="UTF-8">
@@ -27,13 +92,27 @@
   <title>Document</title>
 </head>
 <body>
-  <div class="topnav">
-  <a class="active" href="/">Home</a>
-  <a href="">Game manual</a>
-  <a href="" style="float: right;">Login In/ SIGN UP</a>
-  </div>
 
+
+
+    <div class="navigation-bar">
+
+
+      <div id="navigation-container">
   
+        <img src="http://neonknightsrpg.com/assets/images/logo-neon-knights-header.png">
+  
+        <div class="topnav">
+          <a href="/">Home</a>
+          <a href="">Game manual</a>
+          <a href="" style="float: right;">Login In/ SIGN UP</a>
+        </div>
+
+      </div>
+  <br>
+  <br>
+  <br>
+  <p>jflkajf;kjaklsdjf;kl</p>
 
   <div class="footer">
     <p>PRIVACY POLICY</p>
