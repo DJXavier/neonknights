9691869ec54daa89ebc12b099ceb60c30b07e988
NoelFieThomas <68940464+NoelFieThomas@users.noreply.github.com>
2021-06-22 11:21:49 +1200

    Create Relationships

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+
+    public function users() {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function knights() {
+        return $this->hasMany(Knight::class);
+    }
 }
