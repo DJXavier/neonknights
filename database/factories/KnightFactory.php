b5b19dca64a63432945aa8c7b77d1bf8e9e63dff
NoelFieThomas <68940464+NoelFieThomas@users.noreply.github.com>
2021-06-22 11:23:54 +1200

    Create Factories

---
+++ database/factories/KnightFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Knight;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class KnightFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Knight::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
