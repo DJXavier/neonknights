f44a09201de4387297299da0bc8eff03fa7d618b
NoelFieThomas <68940464+NoelFieThomas@users.noreply.github.com>
2021-06-22 20:46:32 +1200

    Removing Test User
    Not required.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory()->create([
-            'name' => 'testAcc',
-            'email' => 'test@test.com',
-        ]);
-
         \App\Models\User::factory(10)->create();
         \App\Models\Game::factory(10)->create();
         \App\Models\Knight::factory(30)->create();
