5de75fd8c3335e09d829878faaa0aa417d65f28b
Gerard Gomez <gerardalexigomez@yahoo.com>
2021-07-18 03:16:39 +1200

    styling on view and css edits

--- resources/views/knights/character.blade.php
+++ resources/views/knights/character.blade.php
@@ -28,22 +28,22 @@ function rollConstitution() {
 }
 
 </script>
-<!-- <style>
+<style>
     body {
-        background-color: #080325e6;
+        background-color: #080325;
     }
 
-    p {
+    /* p {
         font-size: 22px;
         font: sans-serif;
-    }
+    } */
 
     strong {
         font-size: 22px;
         font: sans-serif;
     }
 
-    h1 {
+    /* h1 {
         
         font-size: 50px;
         color: #ffffff;
@@ -56,9 +56,9 @@ function rollConstitution() {
     div{
         font-size: 22px;
         color: #ffffff;
-    }
+    } */
 
-</style> -->
+</style>
 <head>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -66,13 +66,13 @@ function rollConstitution() {
     <div class="container">
         <div class="row justify-content-center">
         <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">{{ __('Knight Creation')}}</div>
-                <div class="card-body">
+        <div class="card text-white">
+            <div class="card-header text-white">{{ __('Knight Creation')}}</div>
+                <div class="card-body ">
                 
                     <br>
                     <form action="" method="POST">
-                        <div class="form-group row">
+                        <div class="form-group row text-white">
                             <label for="knight" class="col-md-4 col-form-label text-md-right">{{ __('Knight Name') }} </label>
                             <div class="col-md-6"> 
                                 <input type="text" class="form-control " name="character" >
@@ -80,58 +80,61 @@ function rollConstitution() {
                         </div>  
 
                         
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">{{ __('Pronouns' )}} </label>
-                            <div class="form-check form-check-inline">
-                                <input type="radio" value="Male" class="form-check-input" id="male" name="gender">
-                                <label class="form-check-label" for="male">{{ __('He/His') }}</label>
-                            </div>
-                            <div class="form-check form-check-inline col-md-1">
-                                <input type="radio" value="Female" class="form-check-input" id="female" name="gender">
-                                <label class="form-check-label" for="female">{{ __('She/Her') }}</label>
-                            </div>          
-                            <div class="form-check form-check-inline">
-                                <input type="radio" value="Them" class="form-check-input" id="them" name="gender">
-                                <label class="form-check-label" for="them">{{ __('They/Them')}}</label>
+                        <div class="form-group row text-white">
+                            <label class="col-4 text-md-right" >{{ __('Pronouns' )}} </label>
+                            <div class="col-5 text-center"> 
+                                <div class="form-check form-check-inline">
+                                    <input type="radio" value="Male" class="form-check-input" id="male" name="gender">
+                                    <label class="form-check-label" for="male">{{ __('He/His') }}</label>
+                                </div>
+                                <div class="form-check form-check-inline ">
+                                    <input type="radio" value="Female" class="form-check-input" id="female" name="gender">
+                                    <label class="form-check-label" for="female">{{ __('She/Her') }}</label>
+                                </div>          
+                                <div class="form-check form-check-inline">
+                                    <input type="radio" value="Them" class="form-check-input" id="them" name="gender">
+                                    <label class="form-check-label" for="them">{{ __('They/Them')}}</label>
+                                </div>
                             </div>
                         </div>
                         <p></p>
 
-                        <div class="form-group row">
-                            <div class="col-sm-1 justify-content-center text-center">
+                        <div class="form-group row mb-0">
+                            <div class="col-1 offset-md-3">
                                 <i class="fa fa-fw fa-hand-grab-o fa-2x pb-3" style="color: #d92b4c"></i>
                             </div>
-                            <div class="col-sm-10">
-                                <input type="button" value="Roll Strength" id="strength" name="strength" onclick="rollStrength(); this.onclick=null;">
-                                &nbsp;&nbsp;<span id= "showStrength"></span>
+                            <div class="col">
+                                <input type="button" value="Roll Strength" style="width: 150px;" id="strength" name="strength" onclick="rollStrength(); this.onclick=null;">
+                                <span class="offset-1" id= "showStrength"></span>
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-sm-1 justify-content-center text-center">
+                        <div class="form-group row mb-0">
+                            <div class="col-1 offset-md-3">
                                 <i class="fa fa-fw fa-eye fa-2x pb-3" style="color: #d92b4c"></i>
                             </div>
-                            
-                            <div class="col-sm-10">
-                                <input type="button" value="Roll Dexterity" id="dexterity" name="dexterity" onclick="rollDexterity(); this.onclick=null;">
-                                &nbsp;&nbsp;<span id= "showDexterity"></span>
+                            <div class="col">
+                                <input type="button" value="Roll Dexterity" style="width: 150px;" id="dexterity" name="dexterity" onclick="rollDexterity(); this.onclick=null;">
+                                <span class="offset-1" id= "showDexterity"></span>
                             </div>
                                 
                         </div>
             
-                        <div class="form-group row">
-                            <div class="col-sm-1 justify-content-center text-center">
+                        <div class="form-group row mb-0">
+                            <div class="col-1 offset-md-3">
                                 <i class="fa fa-fw fa-balance-scale fa-2x pb-3" style="color: #d92b4c"></i>
                             </div>
-                            <div class="col-sm-10" >
-                                <input type="button" value="Roll Constitution" id="constitution" name="constitution" onclick="rollConstitution(); this.onclick=null;">
-                                &nbsp;&nbsp;<span id= "showConstitution"></span>
+                            <div class="col">
+                                <input type="button" value="Roll Constitution" style="width: 150px;" id="constitution " name="constitution" onclick="rollConstitution(); this.onclick=null;">
+                                <span class="offset-1" id= "showConstitution" ></span>
                             </div>
                         </div>
-                    <br>
-                    
-                    <input type="submit" class="btn btn-primary" value="Create Character" id="create" name="create">
-                        
+                        <br>
+                        <div class="form-group row mb-0">
+                            <div class="col text-center" >
+                            <button type="submit" class="btn btn-primary" style="width: 200px;" id="create">{{ __('Create') }}</button>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
