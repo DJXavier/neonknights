8101219a3561c0b4693e6e23152165195d9378ac
NoelFieThomas <nft4407@aut.ac.nz>
2021-07-30 13:03:31 +1200

    Started Relationship Creation
    Need to test how having two links to the same model would operate.

--- app/Models/Action.php
+++ app/Models/Action.php
@@ -8,4 +8,13 @@ use Jenssegers\Mongodb\Eloquent\Model;
 class Action extends Model
 {
     use HasFactory;
+
+    //Need one relationship for knight taking action, and one for knight to joust against.
+    public function knight() {
+        return $this->embedsOne(Knight::class);
+    }
+
+    public function noblebot() {
+        return $this->embedsOne(Noblebot::class);
+    }
 }
--- app/Models/GameRound.php
+++ app/Models/GameRound.php
@@ -8,4 +8,8 @@ use Jenssegers\Mongodb\Eloquent\Model;
 class GameRound extends Model
 {
     use HasFactory;
+
+    public function weeks() {
+        return $this->embedsMany(Week::class);
+    }
 }
