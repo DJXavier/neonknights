75e619984c1e77097fe94f5fa5b0ce31c88ca70c
NoelFieThomas <nft4407@aut.ac.nz>
2021-08-17 13:20:30 +1200

    Found and Added Registered Function
    Will be used for logging out for email verification.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,4 +71,17 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    /**
+     * The user has been registered.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user) {
+        if (!$user->verified) {
+            //auth()->logout();
+        }
+    }
 }
