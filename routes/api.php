8f1660fa8f51d065a05262fc1cad41fe7fc2815d
Gerard Gomez <gerardalexigomez@yahoo.com>
2021-09-26 18:12:11 +1300

    creating api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/knight', [App\Http\Controllers\KnightController::class, 'store'])->name('knight.store');
+Route::get('/', function () {
+    return view('homepage.homePage');
+});
+
+Route::get('/policy', function () {
+    return view('homepage.privacy_policy');
+});
+
+Route::get('/handbook', function () {
+    return view('homepage.handbook');
+});
+
+Route::get('/character/create/{gameId}', function ($gameId) {
+    return view('knights.character', ['id' => $gameId]);
+});
+
+
