1fa7a0707f5f8dbe2140ca22fc4ed4fb778cacd8
NoelFieThomas <nft4407@aut.ac.nz>
2021-10-05 02:29:44 +1300

    Moved logout button to bottom of dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,13 +113,13 @@
                                     <a class="dropdown-item" href="/display-groups-characters"> 
                                         {{ __('Display Games & Characters') }}</a> 
                                     @endif
+                                    <a class="dropdown-item" href="{{ route('password.change') }}";>
+                                        {{ __('Change Password') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('password.change') }}";>
-                                        {{ __('Change Password') }}
                                     </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
